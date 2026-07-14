b4548e2598285c4cc03ca28b0ede70b2058f09a6
tohoku8141 <sasaki.hirose.q4@tohoku.ac.jp>
2026-07-14 22:04:09 +0900

    Update CORS settings

--- backend/main.py
+++ backend/main.py
@@ -16,7 +16,8 @@ from fastapi.middleware.cors import CORSMiddleware
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=["http://localhost:5173",
+                   "https://youtube-info-analyzer.vercel.app/"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
