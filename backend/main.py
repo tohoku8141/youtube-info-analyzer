609ce4ffa05c855ad25690d4f4a2f7caaf034f22
tohoku8141 <sasaki.hirose.q4@tohoku.ac.jp>
2026-07-14 22:17:15 +0900

    Update CORS

--- backend/main.py
+++ backend/main.py
@@ -16,9 +16,8 @@ from fastapi.middleware.cors import CORSMiddleware
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173",
-                   "https://youtube-info-analyzer.vercel.app/"],
-    allow_credentials=True,
+    allow_origins=["*"],
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
